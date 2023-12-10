afe94bd3890827c4a7f4fb08d35024d846b6c393
Anna <kotlyaranya1771@gmail.com>
2023-12-10 19:01:01 +0200

    workflow

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,3 +1,14 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//     plugins: [react()],
+//     base: "/theBestCarRentalEver",
+//     resolve: {
+//         alias: { src: "/src" },
+//     },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
