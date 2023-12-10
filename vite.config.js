6f0f8a07e5b9be0a5524e7ded7e1f6553a463acb
Anna <kotlyaranya1771@gmail.com>
2023-12-10 17:59:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/theBestCarRentalEver",
-  build: {
-    outDir: "dist",
-    assetsDir: "public",
-  },
+    plugins: [react()],
+    base: "/theBestCarRentalEver",
+    resolve: {
+        alias: { src: "/src" },
+    },
 });
