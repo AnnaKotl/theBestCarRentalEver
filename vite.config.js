3361c6eef7b4b0e0256c43ea309d70047fc83a71
Anna <kotlyaranya1771@gmail.com>
2023-12-11 18:55:40 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/theBestCarRentalEver";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/theBestCarRentalEver",
     resolve: {
         alias: { src: "/src" },
     },
